b90408a62ac8d8865ccb9224d9074405ab028e25
Tejaswi <tejaswi.maryala+62@wavemaker.com>
2019-04-22 10:03:53 +0000

    Initial Push from master

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,14 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "bricks",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Mobile_Localization",
+  "homePage" : "Main",
+  "name" : "Mobile_Localization",
+  "platformType" : "MOBILE",
+  "securityEnabled" : "false",
+  "supportedLanguages" : "af,en,fr,hi-in,te-in",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
